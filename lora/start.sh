0f08d640e33668fc63b72eef72d4bb484f934425
RAKwireless <RAKWireless@github.com>
2019-05-06 14:54:18 +0800

    modify start.sh

--- lora/start.sh
+++ lora/start.sh
@@ -12,8 +12,8 @@ sleep 0.1
 echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
 sleep 0.1
 echo "$SX1301_RESET_BCM_PIN" > /sys/class/gpio/unexport
-#./set_eui.sh
+./set_eui.sh
 sleep 0.2
-#./update_gwid.sh ./local_conf.json
+./update_gwid.sh ./local_conf.json
 sleep 0.5
 ./lora_pkt_fwd
